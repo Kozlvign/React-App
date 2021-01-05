f4f9a5e33d5a96468b728cc7be652558f05cd77a
Igor Nunes <63605496+Kozlvign@users.noreply.github.com>
2021-01-05 09:42:21 -0300

    values jsx can't see

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ function getButtonText() {
 }
 
 const App = () => {
-    const buttonText = 'Click Me!'
+    const buttonText = { text: 'Click Me!' };
 
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
         </div>
     );
 };
