04c9e7f49f66138cb4cf70c32cf8cd0251cf005a
Igor Nunes <63605496+Kozlvign@users.noreply.github.com>
2021-01-05 09:36:50 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 const App = () => {
     const buttonText = 'Click Me!'
-    
+
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
         </div>
     );
 };
