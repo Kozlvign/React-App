ae914863391d333ad72b882dbf040eb73b94842e
Igor Nunes <63605496+Kozlvign@users.noreply.github.com>
2021-01-05 09:44:03 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ function getButtonText() {
 
 const App = () => {
     const buttonText = { text: 'Click Me!' };
+    const style = { backgroundColor: 'blue', color: 'white' }
 
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
+            <button style={style}>{buttonText.text}</button>
         </div>
     );
 };
