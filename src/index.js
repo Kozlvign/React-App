97e17f1d247e743919596d250488e30cb3a037b2
Igor Nunes <63605496+Kozlvign@users.noreply.github.com>
2021-01-05 08:25:48 -0300

    Understanding React

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi there!</div>
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style="background-color: blue; color: white;">Submit</button>
+        </div>
+    );
 };
 
 ReactDOM.render(
